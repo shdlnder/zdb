c3419160537597fdc062cc9f9882f363a7a3de1f
Shadows Go <github.christopher.dahm@pm.me>
2024-05-14 23:05:03 -0400

    Remove unused

--- src/structure/tree.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-// if len > 1, check branches. else check leaves
-pub const Branch = struct {
-    key: u8,
-    branches: std.AutoHashMap(u8, Branch),
-    leaves: std.AutoHashMap(u8, []u8),
-};
